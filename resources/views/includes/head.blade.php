1277f60e1a4a92aba4bff4a688c8555923909f16
Allen Garvey <allengarvey@hotmail.com>
2015-08-27 00:30:26 -0400

    add nav to header

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -9,3 +9,12 @@
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
     </head>
     <body>
+    	<div class="nav_bar">
+    		<nav class='nav container'>
+    			<a href="{{URL::route('home')}}" class="brand">Allen Garvey</a>
+				<ul>
+					<li><a href="{{URL::route('code')}}">Code</a></li>
+					<li><a href="{{URL::route('design')}}">Design</a></li>
+				</ul>
+    		</nav>
+    	</div>
