b4bb838cfaa66fe7ea42282204e9d4a2ec13d22b
Allen Garvey <allengarvey@hotmail.com>
2015-08-29 21:25:04 -0400

    rename code index and show pages to be more general

--- resources/views/pages/code.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.page')
-@section('content')
-    <h1>Code</h1>
-    <ul>
-    	@foreach($codes as $id => $item)
-    		<li><a href="{{URL::route('code.show', $id)}}">{{$item['title']}}</a></li>
-    	@endforeach
-    </ul>
-@stop
--- resources/views/pages/code_item.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.page')
-@section('content')
-    <h1>{{$code['title']}}</h1>
-    <p>{!!$code['blurb']!!}</p>
-@stop
