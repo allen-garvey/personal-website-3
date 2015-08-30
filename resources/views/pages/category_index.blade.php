2aa3ef67113ffaf9340581905aa61bf12dd57bca
Allen Garvey <allengarvey@hotmail.com>
2015-08-29 22:32:01 -0400

    add container classes to category show and index pages

--- resources/views/pages/category_index.blade.php
+++ resources/views/pages/category_index.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.page')
 @section('content')
-    <h1>{{$data['title']}}</h1>
-    <ul>
-    	@foreach($data['items'] as $id => $item)
-    		<li><a href="{{URL::route($data['uri'] .'.show', $id)}}">{{$item['title']}}</a></li>
-    	@endforeach
-    </ul>
+	<main class="container">
+		<h1>{{$data['title']}}</h1>
+	    <ul>
+	    	@foreach($data['items'] as $id => $item)
+	    		<li><a href="{{URL::route($data['uri'] .'.show', $id)}}">{{$item['title']}}</a></li>
+	    	@endforeach
+	    </ul>
+	</main>
 @stop
--- resources/views/pages/category_show.blade.php
+++ resources/views/pages/category_show.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.page')
 @section('content')
-    <h1>{{$item['title']}}</h1>
-    <p>{!!$item['description']!!}</p>
+	<main class="container">
+		<h1>{{$item['title']}}</h1>
+    	<p>{!!$item['description']!!}</p>
+	</main>
 @stop
