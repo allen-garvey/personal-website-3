98fd0ea48ad47850ab31ab69081ad16c1ddeb945
Allen Garvey <allengarvey@hotmail.com>
2015-09-13 15:29:40 -0400

    refactor image comparison box to display inline-block and to use inline images instead of background images so that image urls are not hard-coded

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,14 +6,17 @@
 				<h1 class="site_title">Allen Garvey</h1>
 				<h2 class="site_subtitle">Front-end web developer for the 21<sup>st</sup> century</h2>
 			</hgroup>
-			<div id="comparison">
+			<div class="image_comparison_parent" id="comparison">
 				<figure>
-					<div id="divisor"></div>
+					<div class="image_comparison_container">
+						{!!HTML::image('images/portraits/allen-garvey-art-portrait-crop.png', 'Portrait of Allen Garvey')!!}
+					</div>
+					<div class="image_comparison_container divisor" id="divisor">
+						{!!HTML::image('images/portraits/allen-garvey-code-portrait.png', 'Portrait of Allen Garvey')!!}
+					</div>
 				</figure>
 				<input type="range" min="0" max="100" value="50" id="slider">
 			</div>
-
-			{{-- {!!HTML::image('images/allen-garvey-portrait-composite.png', 'Portrait of Allen Garvey')!!} --}}
 			<div class="lead">
 				<p>Hi, I'm Allen. I do blank and blank.</p>
 			</div>
