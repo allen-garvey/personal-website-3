423d1112ffc3cde0da8d004c9f5f25b4d5faeb09
Allen Garvey <allengarvey@hotmail.com>
2015-09-12 21:08:03 -0400

    edit to remove laravel-elixr and add js compilation tastks

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,28 @@
-var elixir = require('laravel-elixir');
+"use strict";
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var maps = require('gulp-sourcemaps');
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+gulp.task('concatScripts', function(){
+	return gulp.src(['js/image_comparison_slider.js'])
+		.pipe(maps.init())
+		.pipe(concat('app.js'))
+		.pipe(maps.write('./'))
+		.pipe(gulp.dest('./public/scripts'));
 });
+
+gulp.task('minifyScripts', ['concatScripts'], function(){
+	return gulp.src('./public/scripts/app.js')
+		.pipe(uglify())
+		.pipe(rename('app.min.js'))
+		.pipe(gulp.dest('./public/scripts'));
+});
+gulp.task('watchScripts', function(){
+	gulp.watch('js/**/*.js', ['minifyScripts']);
+});
+
+gulp.task('build', ['minifyScripts']);
+gulp.task('default', ['build']);
