cc1181eb285424ec97fff74970ad1e38bc5c10f8
Allen Garvey <allengarvey@hotmail.com>
2015-09-22 01:41:52 -0400

    add documentation for .total div

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
-</div>{{-- end .total div --}}
-<div class="footer">Questions, comments, job offers? Contact <a href="mailto:test@email.com">test@email.com</a></div>
+</div>{{-- end .total div (used to place footer at bottom of the page) --}}
+<div class="footer" id="footer"></div>
 {!!HTML::script('scripts/app.min.js')!!}
 </body>
 </html>
